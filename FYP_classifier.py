127e876efa1dedf4a715b85b1c86619100d0b5b0
Lim Chia Kai <clim0002@student.monash.edu>
2019-04-13 01:20:06 +0800

    Update FYP_classifier.py
    Changes:
    1) Reset index of current and former employee reviews (convenient for line 88 to 93)
    
    2) Split training and testing sets (line 43 to 47)
    
    3) Deleted testing code (line highlighted by red)

--- FYP_classifier.py
+++ FYP_classifier.py
@@ -11,6 +11,7 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from keras.preprocessing.text import Tokenizer
 
 path = "../Desktop/reviews.csv"
 data = pd.read_csv(path)
@@ -31,8 +32,24 @@ print (reviews["employeeType"].value_counts())
 # Separate current employee reviews and former employee reviews
 current = reviews[reviews["employeeType"] == "Current Employee"]
 former = reviews[reviews["employeeType"] == "Former Employee"]
+
+# Reset index
+current = current.reset_index(drop=True)
+former = former.reset_index(drop=True)
+
 print (current.shape)
 print (former.shape)
+
+# Split training and testing set (ratio 8:2)
+currentTrain = current[0: int(current.shape[0] * 0.8)]
+currentTest = current[int(current.shape[0]*0.8):] 
+formerTrain = former[0: int(former.shape[0] * 0.8)]
+formerTest = former[int(former.shape[0]*0.8):] 
+#print (currentTrain)
+#print (currentTest)
+#print (formerTrain)
+#print (formerTest)
+
 
 # Create and compile regular expression patterns into regular expression objects
 # specialCharRemoval for removing non-digit and non-alphabetical characters
@@ -41,10 +58,12 @@ print (former.shape)
 specialCharRemoval = re.compile(r'[^a-z\d ]', re.IGNORECASE)
 replaceDigit = re.compile(r'\d+', re.IGNORECASE)
 
-def preProcessing(text, count):
+def preProcessing(text):
     text = specialCharRemoval.sub('', text)
     text = replaceDigit.sub('', text)
-    text = text.lower().split()
+    text = " ".join(text.lower().split())
+#    if count == 0:
+#        print (text)
         
 #    stopWords = set(stopwords.words("english"))
 #    text = [i for i in text if i not in stopWords]
@@ -56,12 +75,26 @@ def preProcessing(text, count):
 #    if count == 0:
 #        print (stemmed_words)
 #    text = " ".join(stemmed_words)
-    
     return text
-        
-i = 0
-comments = []
-for rev in former["pros"]:
-    comments.append(preProcessing(rev, i))
+
+#i = 0
+currentPosReviews = []
+currentNegReviews = []
+formerPosReviews = []
+formerNegReviews = []
+#for rev in current["pros"]:
+#    currentPosReviews.append(preProcessing(rev))
+
+for i in range(current.shape[0]):
+    if i < former.shape[0]:
+        formerPosReviews.append(preProcessing(former["pros"][i]))
+        formerNegReviews.append(preProcessing(former["cons"][i]))
+    currentPosReviews.append(preProcessing(current["pros"][i]))
+    currentNegReviews.append(preProcessing(current["cons"][i]))
+    
+currentPosReviews = currentPosReviews[0:3]
+print (currentPosReviews)
+tokenizer = Tokenizer(num_words=max_features)
+
     
 
